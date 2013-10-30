cf253a7b0de5f778ce52948fffce286c48c0b072
Mihkel Baranov <mihkel@resac.eu>
2013-10-30 21:15:41 +0200

    Create simple.py

---
+++ simple.py
@@ -0,0 +1,7 @@
+name = raw_input('Enter your name = ');
+
+print name.upper();
+print name.capitalize();
+print name.lower();
+print name.swapcase();
+print name.strip('saM'); #chars to remove s, a and M
