b928a168a553c63a20179e5a476f050698c132cb
Mihkel Baranov <mihkel@resac.eu>
2013-10-30 21:16:15 +0200

    Update simple.py

--- simple.py
+++ simple.py
@@ -4,4 +4,4 @@ print name.upper();
 print name.capitalize();
 print name.lower();
 print name.swapcase();
-print name.strip('saM'); #chars to remove s, a and M
+print name.strip('saM'); #chars to remove are s, a and M :)
