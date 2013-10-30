461a59832fcae10d5a3442ff7427804fa2ccfc47
Mihkel Baranov <mihkel@resac.eu>
2013-10-30 21:21:23 +0200

    markusele

--- simple.py
+++ simple.py
@@ -5,3 +5,33 @@ print name.capitalize();
 print name.lower();
 print name.swapcase();
 print name.strip('saM'); #chars to remove are s, a and M :)
+
+
+Oletame et name:
+  
+name = 'Madis'
+
+upper = name.upper();
+cap = name.capitalize();
+...
+
+if upper == name:
+  print 'Jah'
+else:
+  print 'Ei'
+
+KUVAB EI sest madis ei ole MADIS
+ehk
+madis != MADIS
+
+if cap == name:
+  print 'Jah'
+else:
+  print 'Ei':
+    
+Kuvab "jah"
+SEST?
+
+Madis = Madis
+
+sest cap  on Madis
